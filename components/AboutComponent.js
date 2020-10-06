ecf93b3e960b7b58fabed97b08f75444b5e0b468
ThomasLodato <telodato.16@est.ucab.edu.ve>
2020-10-06 17:53:49 -0400

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -61,10 +62,12 @@ class About extends Component {
         else if (this.props.leaders.errMess) {
             return(
                 <ScrollView>
-                    <History />
-                    <Card title="Corporate Leadership">
-                        <Text>{this.props.leaders.errMess}</Text>
-                    </Card>
+                    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                        <History />
+                        <Card title="Corporate Leadership">
+                            <Text>{this.props.leaders.errMess}</Text>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
@@ -72,14 +75,16 @@ class About extends Component {
         else {
             return(
                 <ScrollView>
-                    <History />
-                    <Card title="Corporate Leadership">
-                        <FlatList
-                            data={this.props.leaders.leaders}
-                            renderItem={renderLeaders}
-                            keyExtractor={item => item.id.toString()}
-                        />
-                    </Card>
+                    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                        <History />
+                        <Card title="Corporate Leadership">
+                            <FlatList
+                                data={this.props.leaders.leaders}
+                                renderItem={renderLeaders}
+                                keyExtractor={item => item.id.toString()}
+                            />
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
